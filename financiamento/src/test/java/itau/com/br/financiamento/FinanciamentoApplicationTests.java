2b6888315a462cf1f1ea0b195df6b6f5d829a088
Paloma Alves <paloma_passere@hotmail.com>
2021-12-02 13:05:10 -0300

    first commit

---
+++ financiamento/src/test/java/itau/com/br/financiamento/FinanciamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package itau.com.br.financiamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanciamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
